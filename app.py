5b9c12ef41be0c70cfb69d2a1ea480b306c35d0a
Anugrah Aidin Yotolembah <108518030+AnugrahAidinYotolembah@users.noreply.github.com>
2024-01-02 22:57:01 +0800

    Update app.py

--- app.py
+++ app.py
@@ -67,15 +67,9 @@ def main():
         unsafe_allow_html=True
     )
 
-    image_path1 = "image/gambar_1.png"  # Ganti dengan path gambar yang sesuai
+    image_path1 = ["image/gambar_1.png", "image/gambar_2.png", "image/gambar_3.png", "image/gambar_4.png"]  # Ganti dengan path gambar yang sesuai
     st.image(image_path1, use_column_width=True)
-    image_path2 = "image/gambar_2.png"  # Ganti dengan path gambar yang sesuai
-    st.image(image_path2, use_column_width=True)
-    image_path3 = "image/gambar_3.png"  # Ganti dengan path gambar yang sesuai
-    st.image(image_path3, use_column_width=True)
-    image_path4 = "image/gambar_4.png"  # Ganti dengan path gambar yang sesuai
-    st.image(image_path4, use_column_width=True)
-
+    
     st.markdown(
         """
         <hr style='border: 2px solid #B81D24;'>
